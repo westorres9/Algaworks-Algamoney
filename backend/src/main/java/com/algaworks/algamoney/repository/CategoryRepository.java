5d0add10152fdc96531146ceca48a44c8d620ef9
westorres9 <wester.torres9@gmail.com>
2022-04-06 11:55:46 -0300

    Create category repository

---
+++ backend/src/main/java/com/algaworks/algamoney/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.algamoney.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algamoney.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
