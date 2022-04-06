35f2e5834383a566d8b38026a01aec8e3c30838a
westorres9 <wester.torres9@gmail.com>
2022-04-06 17:12:36 -0300

    implemented personalized message for exceptions

--- backend/src/main/java/com/algaworks/algamoney/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/algaworks/algamoney/resources/exceptions/ResourceExceptionHandler.java
@@ -2,16 +2,62 @@ package com.algaworks.algamoney.resources.exceptions;
 
 import com.algaworks.algamoney.services.exceptions.DatabaseException;
 import com.algaworks.algamoney.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String userMessage = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+        String developerMessage = ex.getCause().toString();
+        return handleExceptionInternal(ex, new Error(userMessage, developerMessage), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    public static class Error {
+
+        private String userMessage;
+        private String developerMessage;
+
+        public Error(String userMessage, String developerMessage) {
+            this.userMessage = userMessage;
+            this.developerMessage = developerMessage;
+        }
+
+        public String getUserMessage() {
+            return userMessage;
+        }
+
+        public void setUserMessage(String userMessage) {
+            this.userMessage = userMessage;
+        }
+
+        public String getDeveloperMessage() {
+            return developerMessage;
+        }
+
+        public void setDeveloperMessage(String developerMessage) {
+            this.developerMessage = developerMessage;
+        }
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
