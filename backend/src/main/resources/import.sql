5b715d1681612f10181afb270a6fb20708570a1a
westorres9 <wester.torres9@gmail.com>
2022-04-07 08:40:58 -0300

    seedind database with Persons

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -4,3 +4,7 @@ INSERT INTO tb_category (name, created_At) VALUES ('Alimentação', NOW());
 INSERT INTO tb_category (name, created_At) VALUES ('Fármacia', NOW());
 INSERT INTO tb_category (name, created_At) VALUES ('Outros', NOW());
 
+INSERT INTO tb_person (name, active, city, complement, district, number, state, street, zip_code) VALUES ('Wester Torres', true, 'Uberlandia', 'front', 'Morumbi', '271', 'Minas Gerais', 'Rua Arca', '38407-270');
+INSERT INTO tb_person (name, active, city, complement, district, number, state, street, zip_code) VALUES ('Dayane Rosa', true, 'Uberlandia', 'front', 'Morumbi', '271', 'Minas Gerais', 'Rua Arca', '38407-270');
+INSERT INTO tb_person (name, active, city, complement, district, number, state, street, zip_code) VALUES ('Ronaldo Santos', true, 'Uberlandia', 'front', 'Morumbi', '819', 'Minas Gerais', 'Rua Guapeva', '38407-318');
+INSERT INTO tb_person (name, active, city, complement, district, number, state, street, zip_code) VALUES ('Neuza Rosa', true, 'Uberlandia', 'front', 'Morumbi', '819', 'Minas Gerais', 'Rua Guapeva', '38407-318');
