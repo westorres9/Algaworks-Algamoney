08c472bad16efba48d9eaa59e165fb84530ff941
westorres9 <wester.torres9@gmail.com>
2022-04-07 08:00:16 -0300

    Person Repository

---
+++ backend/src/main/java/com/algaworks/algamoney/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.algamoney.repository;
+
+import com.algaworks.algamoney.entities.Person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    
+}
