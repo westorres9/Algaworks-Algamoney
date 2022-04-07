ad8d00adbfc3a499bfca84238f817e6db3b086fc
westorres9 <wester.torres9@gmail.com>
2022-04-07 08:49:25 -0300

    implement findByID in personService

--- backend/src/main/java/com/algaworks/algamoney/services/PersonService.java
+++ backend/src/main/java/com/algaworks/algamoney/services/PersonService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
@@ -14,9 +15,20 @@ public class PersonService {
     @Autowired
     private PersonRepository repository;
 
+    @Transactional(readOnly = true)
     public Page<PersonDTO> findAll(Pageable pageable){
         Page<Person> list = repository.findAll(pageable);
         return list.map(x -> new PersonDTO(x));
     }
+
+    @Transactional(readOnly = true)
+    public PersonDTO findById(Long id) {
+        Person entity = repository.getById(id);
+        return new PersonDTO(entity);
+    }
+
+   
+
+
 
 }
