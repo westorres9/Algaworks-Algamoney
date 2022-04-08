eb7dafdddbc32f2a6ffccba436c1cafcb6edcf7b
westorres9 <wester.torres9@gmail.com>
2022-04-08 12:40:57 -0300

    database seed encrypt password

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -5,8 +5,8 @@ INSERT INTO tb_category (name) VALUES ('Fármacia');
 INSERT INTO tb_category (name) VALUES ('Outros');
 
 
-INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
