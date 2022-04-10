006b31ca2dd5ec52587050e2f13d120c2e6a2011
westorres9 <wester.torres9@gmail.com>
2022-04-10 00:50:48 -0300

    implemented UserDetailsService

--- backend/src/main/java/com/algaworks/algamoney/repositories/UserRepository.java
+++ backend/src/main/java/com/algaworks/algamoney/repositories/UserRepository.java
@@ -7,5 +7,7 @@ import com.algaworks.algamoney.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String emai);
 
 }
--- backend/src/main/java/com/algaworks/algamoney/services/UserService.java
+++ backend/src/main/java/com/algaworks/algamoney/services/UserService.java
@@ -3,11 +3,16 @@ package com.algaworks.algamoney.services;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.ConstraintViolationException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +29,9 @@ import com.algaworks.algamoney.services.exceptions.ResourceNotFoundException;
 import com.algaworks.algamoney.services.exceptions.ValidationException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -114,5 +121,16 @@ public class UserService {
 		} catch (ConstraintViolationException e) {
 			throw new ValidationException("Validation error");
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if( user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("User not found");
+		}
+		logger.info("User found: " + username);
+		return user;
 	}
 }
